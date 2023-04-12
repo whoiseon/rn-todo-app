03a9054a1375a3ffb8fed1ffae2963be22ab14bc
whoiseon <whois__@naver.com>
2023-04-12 21:10:19 +0900

    feat: saved todo item in state

--- App.js
+++ App.js
@@ -6,16 +6,37 @@ import {
   View,
   TextInput,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import { themePalette } from "./lib/colors";
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
 
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
+
+  const addToDo = () => {
+    if (text === "") return;
+
+    // saveToDo
+    const newToDo = {
+      ...toDos,
+      [Date.now()]: {
+        text,
+        completed: false,
+        work: working,
+      },
+    };
+
+    setToDos(newToDo);
+
+    setText("");
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
@@ -44,12 +65,24 @@ export default function App() {
       <View>
         <TextInput
           value={text}
+          onSubmitEditing={addToDo}
           onChangeText={onChangeText}
+          returnKeyType="done"
           placeholderTextColor={themePalette.lightgrey}
           placeholder={working ? "Add a To Do" : "Where do you want to go?"}
           style={styles.input}
         />
       </View>
+      <ScrollView style={styles.toDoList}>
+        {Object.keys(toDos).map((key) => {
+          const toDo = toDos[key];
+          return (
+            <View style={styles.toDo} key={key}>
+              <Text style={styles.toDoText}>{toDo.text}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -77,5 +110,18 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     marginTop: 24,
     fontSize: 18,
+  },
+  toDoList: {
+    marginTop: 32,
+  },
+  toDo: {
+    backgroundColor: themePalette.cardBg,
+    paddingHorizontal: 20,
+    paddingVertical: 24,
+    marginBottom: 16,
+    borderRadius: 8,
+  },
+  toDoText: {
+    color: themePalette.white,
   },
 });
